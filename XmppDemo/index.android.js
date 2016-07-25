4638b7bc7e59184dcacb9da3219b2a3edf9b122c
Kristian Frølund Jørgensen <kristian@tlt.local>
2016-07-25 10:14:09 +0400

    Added RNXMPPPackage to rn getPackages

--- XmppDemo/index.android.js
+++ XmppDemo/index.android.js
@@ -1,53 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class XmppDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import {Router, Switch, Scene} from 'react-native-mobx';
+import Conversation from './components/Conversation';
+import Login from './components/Login';
+import xmpp from './stores/XmppStore';
+// Define all routes of the app
+var XmppDemo = React.createClass({
+  render: function() {
+      return (
+          <Router xmpp={xmpp}>
+            <Scene key="main" tabs component={Switch} selector={()=>!xmpp.logged ? 'login' : 'conversation'}>
+              <Scene key="login" component={Login} title="Login"/>
+              <Scene key="conversation" component={Conversation}/>
+            </Scene>
+          </Router>
+      );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
 
 AppRegistry.registerComponent('XmppDemo', () => XmppDemo);
