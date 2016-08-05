74c46e39a4b8fc8c67735956e9ae81a392603d5e
Tavi <dragulceo@gmail.com>
2016-08-05 04:10:52 +0300

    Add simple send stanza implementation (sends an xml)

--- android/src/main/java/rnxmpp/service/XmppServiceSmackImpl.java
+++ android/src/main/java/rnxmpp/service/XmppServiceSmackImpl.java
@@ -67,11 +67,13 @@ public class XmppServiceSmackImpl implements XmppService, ChatManagerListener, S
                 .setServiceName(serviceName)
                 .setUsernameAndPassword(jidParts[0], password)
                 .setConnectTimeout(3000)
-                .setDebuggerEnabled(true)
+                //.setDebuggerEnabled(true)
                 .setSecurityMode(ConnectionConfiguration.SecurityMode.required);
 
         if (serviceNameParts.length>1){
             confBuilder.setResource(serviceNameParts[1]);
+        } else {
+            confBuilder.setResource(Long.toHexString(Double.doubleToLongBits(Math.random())));
         }
         if (hostname != null){
             confBuilder.setHost(hostname);
@@ -156,9 +158,30 @@ public class XmppServiceSmackImpl implements XmppService, ChatManagerListener, S
         }
     }
 
+    public class StanzaPacket extends org.jivesoftware.smack.packet.Stanza {
+         private String xmlString;
+
+         public StanzaPacket(String xmlString) {
+             super();
+             this.xmlString = xmlString;
+         }
+
+         @Override
+         public XmlStringBuilder toXML() {
+             XmlStringBuilder xml = new XmlStringBuilder();
+             xml.append(this.xmlString);
+             return xml;
+         }
+    }
+
     @Override
     public void sendStanza(String stanza) {
-        throw new UnsupportedOperationException("Can not send stanzas in react-native-xmpp android implementation.");
+        StanzaPacket packet = new StanzaPacket(stanza);
+        try {
+            connection.sendPacket(packet);
+        } catch (SmackException e) {
+            logger.log(Level.WARNING, "Could not send stanza", e);
+        }
     }
 
     @Override
