16fc8d69c7d3b20d8e77226726eb28e710193822
Pavlo Aksonov <aksonov@gmail.com>
2017-01-06 17:25:08 +0100

    restore original android files

--- XmppDemo/android/app/src/main/java/com/xmppdemo/MainApplication.java
+++ XmppDemo/android/app/src/main/java/com/xmppdemo/MainApplication.java
@@ -8,10 +8,11 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
+import rnxmpp.RNXMPPPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,19 +25,14 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+          new RNXMPPPackage()
       );
     }
   };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+      return mReactNativeHost;
   }
 }
