fa7d27efe1c48d00fc3b7def5f8acc32e4c56a26
Andreas Burghardt <andreas.burghardt@antwerpes.de>
2017-08-29 13:31:48 +0200

    createJSModules isn't required anymore in newer React-Native Versions. Deleted @Override

--- android/src/main/java/rnxmpp/RNXMPPPackage.java
+++ android/src/main/java/rnxmpp/RNXMPPPackage.java
@@ -25,7 +25,6 @@ public class RNXMPPPackage implements ReactPackage{
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
