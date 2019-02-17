e9ce78cb443539fb6303b0ca30e54c682d4c896d
MrScorp <vigithvg@gmail.com>
2019-02-17 20:17:41 +0530

    Update Parser.java
    WritableMap.hasKey() method was not working, always returning false. Using HashMap and ArrayList directly to populate the data structure, and the converting it to WritableMap seemed to be the next way forward.

--- android/src/main/java/rnxmpp/utils/Parser.java
+++ android/src/main/java/rnxmpp/utils/Parser.java
@@ -11,7 +11,6 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
@@ -22,6 +21,10 @@ import org.xml.sax.SAXException;
 
 import java.io.IOException;
 import java.io.StringReader;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -34,70 +37,81 @@ public class Parser {
 
     public static WritableMap parse(String xml){
         try {
-            JSONObject jsonObject = new JSONObject();
             Document iqDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new InputSource(new StringReader(xml)));
             Element documentElement = iqDocument.getDocumentElement();
-            WritableMap node = parse(documentElement);
+            WritableMap node = getWritableMap(parse(documentElement));
             return node;
-        } catch (SAXException | ParserConfigurationException | IOException | JSONException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-
-    static WritableMap parse(Node node) throws JSONException {
-        WritableMap writableMap = Arguments.createMap();
-        NamedNodeMap documentElementAttributes = node.getAttributes();
+	
+	static HashMap parse(Node node)
+	{
+		HashMap<String, Object> writableMap = new HashMap<String, Object>();
+		NamedNodeMap documentElementAttributes = node.getAttributes();
 
         if (documentElementAttributes != null){
             for (int i = 0; i < documentElementAttributes.getLength(); i++){
                 Node attribute = documentElementAttributes.item(i);
-                writableMap.putString(attribute.getNodeName(), attribute.getNodeValue());
+                writableMap.put(attribute.getNodeName(), attribute.getNodeValue());
             }
         }
-
-        if (node.hasChildNodes()){
+		
+		if (node.hasChildNodes()){
             NodeList childNodes = node.getChildNodes();
-            for (int i = 0; i < childNodes.getLength(); i++) {
+            for (int i = 0; i < childNodes.getLength(); i++){
                 Node childNode = childNodes.item(i);
-                if (writableMap.hasKey(childNode.getNodeName()) && writableMap.getType(childNode.getNodeName()).equals(ReadableType.Map)){
-                    WritableArray childArray = Arguments.createArray();
-                    childArray.pushMap(Arguments.fromBundle(Arguments.toBundle(writableMap.getMap(childNode.getNodeName()))));
-                    childArray.pushMap(parse(childNode));
-                    writableMap.putArray(childNode.getNodeName(), childArray);
-                } else if (writableMap.hasKey(childNode.getNodeName()) && writableMap.getType(childNode.getNodeName()).equals(ReadableType.Array)){
-                    WritableArray writableArray = fromArray(writableMap.getArray(childNode.getNodeName()));
-                    writableArray.pushMap(parse(childNode));
-                    writableMap.putArray(childNode.getNodeName(), writableArray);
-                } else {
-                    NodeList checkTextChildNodes = childNode.getChildNodes();
+                if (writableMap.containsKey(childNode.getNodeName())){
+					Object val = writableMap.get(childNode.getNodeName());
+					if (val instanceof HashMap){
+						ArrayList<Object> childArray = new ArrayList<Object>();
+						childArray.add(val);
+						childArray.add(parse(childNode));
+						writableMap.put(childNode.getNodeName(), childArray);
+					}else if (val instanceof ArrayList){
+						((ArrayList)val).add(parse(childNode));
+					}
+				}else{
+					NodeList checkTextChildNodes = childNode.getChildNodes();
                     if (checkTextChildNodes.getLength() == 1 && checkTextChildNodes.item(0).getNodeName().equals("#text")){
-                        writableMap.putString(childNode.getNodeName(), checkTextChildNodes.item(0).getNodeValue());
+                        writableMap.put(childNode.getNodeName(), checkTextChildNodes.item(0).getNodeValue());
                     }else{
-                        writableMap.putMap(childNode.getNodeName(), parse(childNode));
-                    }
-                }
-            }
-        }
-        return writableMap;
-    }
+                        writableMap.put(childNode.getNodeName(), parse(childNode));
+					}
+				}
+			}
+		}
+		return writableMap;
+	}
 
-    static WritableArray fromArray(ReadableArray readableArray){
-        WritableArray newArray = Arguments.createArray();
-        for (int i = 0; i < readableArray.size(); i++) {
-            ReadableType type = readableArray.getType(i);
-            if (type.equals(ReadableType.Map)){
-                newArray.pushMap(Arguments.fromBundle(Arguments.toBundle(readableArray.getMap(i))));
-            }else if(type.equals(ReadableType.Array)){
-                newArray.pushArray(fromArray(readableArray.getArray(i)));
-            }else if(type.equals(ReadableType.Boolean)){
-                newArray.pushBoolean(readableArray.getBoolean(i));
-            }else if(type.equals(ReadableType.Number)){
-                newArray.pushInt(readableArray.getInt(i));
-            }else if(type.equals(ReadableType.String)){
-                newArray.pushString(readableArray.getString(i));
-            }
-        }
-        return newArray;
-    }
+	static WritableArray getWritableArray(ArrayList<Object> list){
+		WritableArray writableArray = Arguments.createArray();
+		for (Object val : list){
+			if (val instanceof HashMap){
+				writableArray.pushMap(getWritableMap((HashMap)val));
+			}else if (val instanceof ArrayList){
+				writableArray.pushArray(getWritableArray((ArrayList)val));
+			}else{
+				writableArray.pushString(String.valueOf(val));
+			}
+		}
+		return writableArray;
+	}
+	
+	static WritableMap getWritableMap(HashMap<String, Object> hashMap){
+		WritableMap writableMap = Arguments.createMap();
+		for (Map.Entry<String, Object> entry : hashMap.entrySet()){
+			Object val = entry.getValue();
+			if (val instanceof HashMap){
+				writableMap.putMap(entry.getKey(), getWritableMap( (HashMap<String, Object>)val));
+			}else if (val instanceof ArrayList){
+				writableMap.putArray(entry.getKey(), getWritableArray( (ArrayList<Object>) val));
+			}else{
+				writableMap.putString(entry.getKey(), String.valueOf(val));
+			}
+		}
+		return writableMap;
+	}
 }
